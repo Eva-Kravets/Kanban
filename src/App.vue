f14e40af9e112bc16b31763f8f2dff8d4641fb5f
Eva-Kravets <71512957+Eva-Kravets@users.noreply.github.com>
2021-05-14 18:57:31 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="page-header">
+      <h1>Канбан-доска</h1>
+      <p class="lead"> Помогает наглядно представить задачи, ограничить объем незавершенной работы и добиться максимальной эффективности</p>
+    </div>
+
+    <menu-bar></menu-bar>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MenuBar from '@/components/MenuBar';
+export default {
+  name: 'app',
+  components: {
+    'menu-bar': MenuBar
+  }
+};
+</script>
+
+<style>
+.page-header h1 {
+  font-weight: 300;
+}
+.card {
+  margin-bottom: 10px;
+}
+.card:last-child {
+  margin-bottom: 25px;
+}
+.card-block {
+  padding: 15px;
+}
+</style>
---
+++ src/components/NewItemForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="add-item">
+        <form action="#" method="post" v-on:submit.prevent="submitForm">
+            <input type="text" v-model="itemText" placeholder="Добавь новую задачу в список">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NewItemForm',
+  data() {
+    return {
+      itemText: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.itemText) {
+        this.$store.commit('addItem', {
+          text: this.itemText
+        });
+        this.itemText = '';
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.add-item input {
+  outline: none;
+  border: 0;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+  width: 100%;
+  transition: all 0.25s;
+  background: inherit;
+  color: white;
+}
+.add-item input:focus {
+  border-bottom-color: rgba(255, 255, 255, 1);
+}
+.add-item {
+  margin: 30px 0;
+}
+</style>
+© 2021 GitHub, Inc.
